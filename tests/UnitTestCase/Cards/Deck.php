6ce21fee9d94b5f6f6b9671450775f77c42b57fc
luke.visinoni@gmail.com <luke.visinoni@gmail.com@ee298eeb-045b-e238-35b8-7e83fa81d589>
2012-11-08 21:13:58 +0000

    Added test for Cards\Deck::deal()

--- tests/UnitTestCase/Cards/Deck.php
+++ tests/UnitTestCase/Cards/Deck.php
@@ -30,10 +30,33 @@ class Deck extends \UnitTestCase {
     
     }
     
+    /**
+     * Test that it is possible to count() the deck
+     */
     public function testCountableInterface() {
     
         $deck = new Cards\Deck;
         $this->assertEqual(count($deck), 52);
+    
+    }
+    
+    /**
+     * Test that the deal() method removes a card from the deck and returns it
+     */
+    public function testDealCardRemovesCardFromDeck() {
+    
+        $deck = new Cards\Deck;
+        $card = $deck->deal();
+        $this->assertEqual(count($deck), 51);
+        $this->assertIsA($card, 'Cards\\Card');
+    
+    }
+    
+    public function testDealCardReturnsTopCard() {
+    
+        $deck = new Cards\Deck; // Since deck isn't shuffled, top card should be 2 of clubs
+        $card = $deck->deal();
+        $this->assertEqual($card->__toString(), '2 of clubs');
     
     }
 
