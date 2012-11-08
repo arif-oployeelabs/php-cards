85ab4e23c4f0d884f95fd50e3ff9c7ae670012ec
luke.visinoni@gmail.com <luke.visinoni@gmail.com@ee298eeb-045b-e238-35b8-7e83fa81d589>
2012-11-08 21:13:58 +0000

    Added test for Cards\Deck::deal()

--- trunk/lib/Cards/Deck.php
+++ trunk/lib/Cards/Deck.php
@@ -22,12 +22,27 @@ class Deck implements \Countable {
 
     const SUIT_SPADES = 'spades';
     
+    /**
+     * @var array The available suits for this deck
+     */
     protected $_suits = array(self::SUIT_CLUBS, self::SUIT_DIAMONDS, self::SUIT_HEARTS, self::SUIT_SPADES);
     
+    /**
+     * @var array The available values for each suit
+     */
     protected $_values = array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A');
     
+    /**
+     * @var array The available cards in the deck
+     */
     protected $_cards = array();
     
+    /**
+     * Class constructor
+     * Creates all the available cards in the deck and optionally shuffles them
+     *
+     * @param boolean Set to false if you do not want the deck shuffled
+     */
     public function __construct($shuffle = true) {
     
         foreach ($this->_suits as $suit) {
@@ -39,9 +54,26 @@ class Deck implements \Countable {
     
     }
     
+    /**
+     * Get the amount of cards available in the deck
+     *
+     * @return integer The amount of cards in the deck
+     */
     public function count() {
     
         return count($this->_cards);
+    
+    }
+    
+    /**
+     * Remove the top card in the deck and return it
+     *
+     * @return Cards\Card The top card in the deck
+     */
+    public function deal() {
+    
+        $top = array_shift($this->_cards);
+        return $top;
     
     }
 
